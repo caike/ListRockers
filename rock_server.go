19bc3d81288332a7bae431804525537bf6e552b6
Carlos Souza <carloshrsouza@gmail.com>
2014-07-21 19:19:38 -0400

    Respond from HTTP.

--- rock_server.go
+++ rock_server.go
@@ -3,9 +3,16 @@ package main
 import (
 	"db/people"
 	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
+	http.HandleFunc("/", rockers)
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+}
+
+func rockers(response http.ResponseWriter, request *http.Request) {
 	people := people.FindAll()
-	fmt.Println(people)
+	fmt.Fprint(response, people)
 }
